814bbb6ef3245c4fbe2ee76df33947f499679db6
Rostislav <hulivets.rostislav@gmail.ccom>
2019-07-25 23:44:35 +0300

    Updates

---
+++ precache-manifest.f5d709255ed6394d8136eb566da10627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623dd7ff4eba97b6e60f2a6d8526a247",
+    "url": "/mag-blog/index.html"
+  },
+  {
+    "revision": "f201f34cbd5b396101c1",
+    "url": "/mag-blog/static/css/main.3c309108.chunk.css"
+  },
+  {
+    "revision": "ee5aa34d21e64a035b0d",
+    "url": "/mag-blog/static/js/2.d13010e9.chunk.js"
+  },
+  {
+    "revision": "f201f34cbd5b396101c1",
+    "url": "/mag-blog/static/js/main.147c7857.chunk.js"
+  },
+  {
+    "revision": "461366dd29f2a1f3d4e9",
+    "url": "/mag-blog/static/js/runtime~main.d4c99942.js"
+  }
+]);
